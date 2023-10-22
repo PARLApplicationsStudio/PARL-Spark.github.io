449718118bcd8a03cd0b0a3ecb2594f33bf4abf3
PARLApplicationsStudio <142746981+PARLApplicationsStudio@users.noreply.github.com>
2023-10-22 16:49:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// Your Firebase configuration here
 const firebaseConfig = {
     apiKey: "AIzaSyC5zupnLXBgT5sOhh8-yAgGUOgvmpTMGFc",
     authDomain: "pspark-34dc3.firebaseapp.com",
-    databaseURL: "https://pspark-34dc3-default-rtdb.firebaseio.com",
     projectId: "pspark-34dc3",
     storageBucket: "pspark-34dc3.appspot.com",
     messagingSenderId: "905920434441",
-    appId: "1:905920434441:web:27dfe76c45c1eaa40243d8",
-    measurementId: "G-V2FWS1BB6E"
+    appId: "1:905920434441:web:27dfe76c45c1eaa40243d8"
 };
 
 // Initialize Firebase
