e022daeec48c71cd33a0ef10f58dd111ff16c5db
PARLApplicationsStudio <142746981+PARLApplicationsStudio@users.noreply.github.com>
2023-10-22 17:40:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+import "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js"; // Add this line to import the authentication module
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC5zupnLXBgT5sOhh8-yAgGUOgvmpTMGFc",
-    authDomain: "pspark-34dc3.firebaseapp.com",
-    databaseURL: "https://pspark-34dc3-default-rtdb.firebaseio.com",
-    projectId: "pspark-34dc3",
-    storageBucket: "pspark-34dc3.appspot.com",
-    messagingSenderId: "905920434441",
-    appId: "1:905920434441:web:27dfe76c45c1eaa40243d8",
+  apiKey: "AIzaSyC5zupnLXBgT5sOhh8-yAgGUOgvmpTMGFc",
+  authDomain: "pspark-34dc3.firebaseapp.com",
+  databaseURL: "https://pspark-34dc3-default-rtdb.firebaseio.com",
+  projectId: "pspark-34dc3",
+  storageBucket: "pspark-34dc3.appspot.com",
+  messagingSenderId: "905920434441",
+  appId: "1:905920434441:web:27dfe76c45c1eaa40243d8",
+  measurementId: "G-V2FWS1BB6E"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Add an event listener to handle user login
 document.getElementById('login-button').addEventListener('click', () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-    // Sign in with email and password
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // User signed in successfully
-            const user = userCredential.user;
-            console.log('User signed in:', user);
-            // Redirect to a different page or perform other actions upon successful login.
-        })
-        .catch((error) => {
-            // Handle sign-in error
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error('Sign-in error:', errorCode, errorMessage);
-            // You can display an error message to the user or take other appropriate actions.
-        });
+  // Sign in with email and password
+  firebase.auth().signInWithEmailAndPassword(email, password) // Fix the method name
+    .then((userCredential) => {
+      // User signed in successfully
+      const user = userCredential.user;
+      console.log('User signed in:', user);
+      // Redirect to a different page or perform other actions upon successful login.
+    })
+    .catch((error) => {
+      // Handle sign-in error
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error('Sign-in error:', errorCode, errorMessage);
+      // You can display an error message to the user or take other appropriate actions.
+    });
 });
